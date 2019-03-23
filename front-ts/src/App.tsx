9179be3c4f2ee537ce3d538b87ad6a6c8dc32616
Al <al@localhost>
2019-03-23 22:22:01 +0300

    TS add router

--- front-ts/src/App.tsx
+++ front-ts/src/App.tsx
@@ -1,26 +1,17 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Hello from "./components/Hello";
+import Room from "./components/Room";
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <Router>
+                <Header/>
+                <Route exact path='/' component={Hello}/>
+                <Route path={`/:roomId`} component={Room}/>
+            </Router>
         );
     }
 }
---
+++ front-ts/src/components/Room.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Room extends Component {
+    render() {
+        return (
+            <div>
+                Room
+            </div>
+        );
+    }
+}
+
+export default Room;

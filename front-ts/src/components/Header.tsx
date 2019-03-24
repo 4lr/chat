a0ffb92e302254f20e9d4390468de7292425c34a
Al <al@localhost>
2019-03-24 14:40:19 +0300

    [TS] add proto room

--- front-ts/src/components/Header.tsx
+++ front-ts/src/components/Header.tsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import './Header.css';
 import logo from './logo.svg';
 
-class Header extends Component {
+class Header extends PureComponent {
     render() {
         return (
             <header className="App-header">
--- front-ts/src/components/Room.tsx
+++ front-ts/src/components/Room.tsx
@@ -1,10 +1,57 @@
-import React, {Component} from 'react';
+import React, {ChangeEvent, PureComponent} from 'react';
+import uuidv4 from 'uuid/v4';
 
-class Room extends Component {
+type Message = {
+    id: string,
+    body: string
+};
+
+export type Props = {
+    roomId: string
+};
+
+export type State = {
+    newMessageText: string,
+    messages: Message[]
+};
+
+const handleChange = Symbol();
+const handleSubmit = Symbol();
+
+class Room extends PureComponent<Props, State> {
+
+    state = {
+        newMessageText: '',
+        messages: []
+    };
+
+    [handleChange] = (ev: ChangeEvent<HTMLInputElement>) => {
+        this.setState({newMessageText: ev.target.value});
+    };
+
+    [handleSubmit] = () => {
+        const messages: Message[] = this.state.messages;
+        const newMessage: Message = {id: uuidv4(), body: this.state.newMessageText};
+        messages.push(newMessage);
+        this.setState({newMessageText: '', messages: messages});
+    };
+
     render() {
         return (
             <div>
-                Room
+                <div>
+                    Room {this.props.roomId}
+                </div>
+                <div>
+                    {this.state.messages.map((message: Message) => <div key={message.id}>{message.body}</div>) }
+                </div>
+                <div>
+                    <input
+                        onChange={this[handleChange]}
+                        value={this.state.newMessageText}
+                    />
+                    <button onClick={this[handleSubmit]}>Send</button>
+                </div>
             </div>
         );
     }
